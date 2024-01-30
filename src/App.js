869425669049ac738d146d1db4901a21215105bf
LegolasArkantos <unasaamir101@gmail.com>
2024-01-30 22:16:14 +0500

    added routes for signup pages

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import Login from "./pages/Login";
 import StudentHomePage from "./pages/StudentHomePage";
 import LandingPage from "./pages/LandingPage";
 import HomePageLayout from "./pages/HomePageLayout";
+import SignUpPage1 from "./pages/SignupPage1";
+import SignUpPage2 from "./pages/SignupPage2";
+import SignUpPage3 from "./pages/SignupPage3";
 
 function App() {
   return (
@@ -15,10 +18,15 @@ function App() {
       <Route element={<PublicRouteLogin />}>
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/signup1" element={<SignUpPage1 />} />
+        <Route path="/signup2" element={<SignUpPage2 />} />
+        <Route path="/signup3" element={<SignUpPage3 />} />
+
       </Route>
 
       <Route element={<PersistentLogin />}>
         <Route path="/home-page" element={<HomePageLayout />}>
+
           {/* Teacher routes */}
           <Route element={<RequiredAuth allowedRole="Teacher" />}></Route>
 
@@ -26,6 +34,7 @@ function App() {
           <Route element={<RequiredAuth allowedRole="Student" />}>
             <Route index element={<StudentHomePage />} />
           </Route>
+
         </Route>
 
         {/* admin routes */}
