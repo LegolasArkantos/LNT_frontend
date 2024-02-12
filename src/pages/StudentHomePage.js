9d7fc2228f08fbf59bbe5fbcddb8ef02067a7c53
LegolasArkantos <unasaamir101@gmail.com>
2024-02-12 19:37:08 +0500

    added profile links

--- src/pages/StudentHomePage.js
+++ src/pages/StudentHomePage.js
@@ -102,7 +102,9 @@ const StudentHomePage = () => {
     var slider = document.getElementById(id);
     slider.scrollLeft += 300; 
   };
-  
+  const handleTeacherClick = (teacherId) => {
+    navigate('/student-home-page/StudentProfileSecondary', { state: { teacherId, otherRole: "Teacher" } });
+  };
 
   return (
     <div>
@@ -178,7 +180,12 @@ const StudentHomePage = () => {
                       {"Price: " + session.sessionPrice}
                     </p>
                     <p class="mb-3 text-gray-700 text-xl dark:text-gray-400">
+                    <button
+                    className="text-blue-500 hover:underline"
+                    onClick={() => handleTeacherClick(session.teacher)}
+                     >
                       {"Teacher: " + session.teacherName}
+                      </button>
                     </p>
                     <p class="mb-3 text-gray-700 text-xl dark:text-gray-400">
                       {"Start: " + session.startTime}
@@ -243,6 +250,7 @@ const StudentHomePage = () => {
                   </a>
                   <div class="p-5 text-wrap">
                     <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                    
                       {teacher.firstName } {teacher.lastName}
                     </h5>
                     <p class="mb-3 font-semibold text-gray-900 text-lg dark:text-gray-400">
@@ -252,7 +260,12 @@ const StudentHomePage = () => {
                       href="#"
                       class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-teal-500 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                     >
+                      <button
+                    className=" hover:underline"
+                    onClick={() => handleTeacherClick(teacher._id)}
+                     >
                       View
+                      </button>
                     </a>
                   </div>
                 </div>
