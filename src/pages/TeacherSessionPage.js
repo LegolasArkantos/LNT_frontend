6830cd2d4e794b39e85569305e4eefc5452ad46c
Uzair Ali <uzairalinusrat@gmail.com>
2024-03-17 23:26:46 +0500

    added leave video call functionality

--- src/pages/TeacherSessionPage.js
+++ src/pages/TeacherSessionPage.js
@@ -143,7 +143,6 @@ const TeacherSessionsPage = ({socket}) => {
   };
 
   const handleJoinVideoCall = (roomID) => {
-    socket.emit("join:video-call", {roomID});
     navigate('/teacher-home-page/live-session', { state: {roomID, userType: "Teacher"}});
   }
 
