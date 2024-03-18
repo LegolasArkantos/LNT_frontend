c01d71328b80db44d7385a15f5e4f1cc2b65315c
LegolasArkantos <unasaamir101@gmail.com>
2024-03-18 23:05:40 +0500

    changes

--- src/pages/TeacherProgress.js
+++ src/pages/TeacherProgress.js
@@ -6,7 +6,6 @@ const AssignmentProgress = () => {
   const [assignments, setAssignments] = useState([]);
 
   useEffect(() => {
-    // Fetch assignment progress data from the backend
     const fetchAssignmentProgress = async () => {
       try {
         const response = await apiPrivate.get('/progress/getAssignmentData');
@@ -19,21 +18,19 @@ const AssignmentProgress = () => {
     fetchAssignmentProgress();
   }, []);
 
-  // Function to calculate average score percentage for an assignment
   const calculateAverageScore = (total, grades) => {
     if (grades.length === 0) return 0;
     const totalGrades = grades.reduce((acc, grade) => acc + grade, 0);
     return (totalGrades / (grades.length * total)) * 100;
   };
 
-  // Prepare data for the chart
   useEffect(() => {
     const chartData = {
       series: [{
         data: assignments.map(assignment => {
           return {
-            x: assignment.title, // Assignment title on x-axis
-            y: calculateAverageScore(assignment.total, assignment.grades) // Average score percentage on y-axis
+            x: assignment.title, 
+            y: calculateAverageScore(assignment.total, assignment.grades) 
           };
         })
       }],
@@ -44,7 +41,7 @@ const AssignmentProgress = () => {
       plotOptions: {
         bar: {
           borderRadius: 4,
-          horizontal: false // Vertical bar chart
+          horizontal: false 
         }
       },
       dataLabels: {
@@ -54,7 +51,7 @@ const AssignmentProgress = () => {
         type: 'category',
         categories: assignments.map(assignment => assignment.title),
         labels: {
-          rotate: -45, // Rotate x-axis labels for better visibility
+          rotate: -45, 
           style: {
             fontSize: '12px'
           }
@@ -66,13 +63,12 @@ const AssignmentProgress = () => {
         tickAmount: 10,
         labels: {
           formatter: function (value) {
-            return value.toFixed(0) + '%'; // Format y-axis labels as percentage
+            return value.toFixed(0) + '%'; 
           }
         }
       }
     };
 
-    // Render the chart
     const chart = new ApexCharts(document.querySelector("#chart"), chartData);
     chart.render();
 
@@ -80,7 +76,7 @@ const AssignmentProgress = () => {
     return () => {
       chart.destroy();
     };
-  }, [assignments]); // Re-render chart when assignments change
+  }, [assignments]); 
 
   return (
     <div className="card" style={{ width: '50%', height: '50vh' }}>
