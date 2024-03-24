6a023e1a1c3a78901c2b7caf2dd3ee23716e452c
LegolasArkantos <unasaamir101@gmail.com>
2024-03-24 23:04:10 +0500

    added path for career

--- src/components/TeacherHomePageNavBar.js
+++ src/components/TeacherHomePageNavBar.js
@@ -1,7 +1,9 @@
-import { Link } from "react-router-dom";
+import { Link ,useLocation} from "react-router-dom";
 import logo from "../assets/l-t-high-resolution-logo-transparent.png";
 
 const TeacherHomePageNavBar = (props) => {
+  const location = useLocation();
+
   return (
     <nav class="top-0 z-20 fixed w-full bg-teal-300 border-gray-200 dark:bg-gray-900">
       <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
@@ -15,21 +17,21 @@ const TeacherHomePageNavBar = (props) => {
         >
           <ul class="flex flex-col text-xl font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-teal-300 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-teal-300 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
-              <a
-                href="#"
-                class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
+            <Link
+                to="/teacher-home-page"
+                className={`block py-2 px-3   rounded md:bg-transparent  md:p-0  ${location.pathname.startsWith("/student-home-page") && !location.pathname.endsWith("/ai-career")  ? 'text-blue-700' : 'text-gray-900'}`}
                 aria-current="page"
               >
                 Learn
-              </a>
+              </Link>
             </li>
             <li>
-              <a
-                href="#"
-                class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+            <Link
+                className={`block py-2 px-3 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 ${location.pathname == "/teacher-home-page/career" ? 'text-blue-700' : 'text-gray-900'}`}
+                to="/teacher-home-page/career"
               >
                 Career
-              </a>
+              </Link>
             </li>
             <li className="">
               <Link
