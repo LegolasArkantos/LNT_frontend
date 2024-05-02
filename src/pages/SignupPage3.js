b369429fd5bb03f4e323d62a0b71338bbefb2e65
Uzair Ali <uzairalinusrat@gmail.com>
2024-05-03 03:29:21 +0500

    improved signup ui

--- src/pages/SignupPage3.js
+++ src/pages/SignupPage3.js
@@ -1,11 +1,10 @@
 import { useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import logo from '../assets/l-t-high-resolution-logo-transparent.png';
+import image from "../assets/signuppage2image.png"
 import { api } from "../services/api";
 import { getStorage,ref, uploadBytes, getDownloadURL } from 'firebase/storage';
 import app from '../services/firebase'
-
-
 
 // Initialize Firebase Storage
 const storage = getStorage(app);
@@ -13,81 +12,6 @@ const storage = getStorage(app);
 const SignUpPage3 = () => {
   const navigate = useNavigate();
   const location = useLocation();
-
-  const [progress, setProgress] = useState(2);
-  const [personality, setPersonality] = useState(new Array(10).fill(0));
-  const [errorMessage, setErrorMessage] = useState("");
-
-  // Extracting previous data from location state
-  const { email, password, firstName, lastName, role, educationalCredential, educationalLevel, profilePicture, aboutMe,teacherFiles} = location.state || {};
-  console.log(profilePicture)
-
-  const submit = async (e) => {
-  e.preventDefault();
-
-  if (personality.includes(0)) {
-    setErrorMessage("Please answer all personality questions.");
-    return;
-  }
-
-  try {
-    const teacherFilesArray = Array.from(teacherFiles);
-
-    // Upload each file to Firebase Storage and get download URLs
-    const fileUploadPromises = teacherFilesArray.map(async (file) => {
-      const storageRef = ref(storage, file.name);
-      await uploadBytes(storageRef, file);
-      const downloadURL = await getDownloadURL(storageRef);
-      return { fileName: file.name, fileUrl: downloadURL };
-    });
-
-    const uploadedFiles = await Promise.all(fileUploadPromises);
-    console.log("Uploaded files:", uploadedFiles);
-
-    const payload = {
-      email,
-      password,
-      firstName,
-      lastName,
-      role,
-      educationalCredential,
-      educationalLevel,
-      personality,
-      profilePicture,
-      aboutMe,
-      credentialFiles: uploadedFiles, // Include credentialFiles in payload
-    };
-
-    console.log("Payload:", payload);
-
-    // Make API request to signup endpoint
-    const response = await api.post("/auth/signup", payload);
-
-    console.log("API Response:", response);
-
-    // Update progress bar and navigate after a delay
-    setProgress(progress + 1);
-
-    setTimeout(() => {
-      navigate("/login");
-    }, 1000);
-
-    // Reset error message
-    setErrorMessage("");
-  } catch (error) {
-    // Handle errors
-    console.error("Error:", error);
-    setErrorMessage("Failed to create an account. Please try again.");
-  }
-};
-
-  const handleOptionChange = (questionIndex, optionIndex) => {
-    setPersonality((prevPersonality) => {
-      const newPersonality = [...prevPersonality];
-      newPersonality[questionIndex] = optionIndex + 1; // Store the selected option (1, 2, or 3)
-      return newPersonality;
-    });
-  };
 
   const questionList = [
     {
@@ -172,46 +96,147 @@ const SignUpPage3 = () => {
     }
   ];
 
+  const [progress, setProgress] = useState(2);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [personality, setPersonality] = useState(Array(questionList.length).fill(0));
+  const [currentQuestionChecked, setCurrentQuestionChecked] = useState(false);
+
+  const handleOptionChange = (questionIndex, optionIndex) => {
+    const newPersonality = [...personality];
+    newPersonality[questionIndex] = optionIndex + 1;
+    setCurrentQuestionChecked(true);
+    setPersonality(newPersonality);
+  };
+
+  const goToNextQuestion = () => {
+    if (currentQuestionChecked) {
+      setCurrentQuestionChecked(false);
+      setCurrentQuestionIndex(currentQuestionIndex + 1);
+    }
+    
+  };
+
+  // Extracting previous data from location state
+  const { email, password, firstName, lastName, role, educationalCredential, educationalLevel, profilePicture, aboutMe,teacherFiles} = location.state || {};
+
+  const submit = async (e) => {
+    e.preventDefault();
+    
+    if (currentQuestionChecked) {
+      if (personality.includes(0)) {
+        setErrorMessage("Please answer all personality questions.");
+        return;
+      }
+  
+      try {
+        const teacherFilesArray = Array.from(teacherFiles);
+  
+        // Upload each file to Firebase Storage and get download URLs
+        const fileUploadPromises = teacherFilesArray.map(async (file) => {
+          const storageRef = ref(storage, file.name);
+          await uploadBytes(storageRef, file);
+          const downloadURL = await getDownloadURL(storageRef);
+          return { fileName: file.name, fileUrl: downloadURL };
+        });
+  
+        const uploadedFiles = await Promise.all(fileUploadPromises);
+        console.log("Uploaded files:", uploadedFiles);
+  
+        const payload = {
+          email,
+          password,
+          firstName,
+          lastName,
+          role,
+          educationalCredential,
+          educationalLevel,
+          personality,
+          profilePicture,
+          aboutMe,
+          credentialFiles: uploadedFiles, // Include credentialFiles in payload
+        };
+  
+        console.log("Payload:", payload);
+  
+        // Make API request to signup endpoint
+        const response = await api.post("/auth/signup", payload);
+  
+        console.log("API Response:", response);
+  
+        // Update progress bar and navigate after a delay
+        setProgress(progress + 1);
+  
+        setTimeout(() => {
+          navigate("/login");
+        }, 1000);
+  
+        // Reset error message
+        setErrorMessage("");
+      } catch (error) {
+        // Handle errors
+        console.error("Error:", error);
+        setErrorMessage("Failed to create an account. Please try again.");
+      }
+    }
+
+    
+  };
+
   return (
-    <div className="w-full bg-white rounded-lg min-h-screen shadow dark:border dark:bg-gray-800 dark:border-gray-700 transition-all duration-1000 ease-in-out">
-      <div className="p-10">
-        <div className="flex mb-10 items-center justify-center space-x-3 rtl:space-x-reverse">
-          <img src={logo} alt="Logo" className="h-8" />
+    <div className="w-full bg-white rounded-lg h-screen shadow dark:border dark:bg-gray-800 dark:border-gray-700 transition-all duration-1000 ease-in-out">
+  <div className="w-full z-20 h-full flex bg-[#7179C6] absolute inset-0">
+    <div className="w-2/5 h-full flex items-center justify-center">
+      <div className="flex-col">
+        <div className="flex items-center mb-10 justify-center">
+          <h className="text-3xl font-semibold text-white">
+            Step 3
+          </h>
         </div>
+        <img src={image} class="w-85 h-70" />
+      </div>
+    </div>
+  </div>
 
-        {/* Progress Bar */}
-        <div className="mb-5 relative pt-1 flex-col mb-2 items-center justify-between ">
-          <ul className="flex relative pt-1">
-            {[1, 2, 3].map((step) => (
-              <div
-                key={step}
-                className={`w-full bg-gray-300 rounded-full h-2 transition-all duration-1000 ease-in-out ${
-                  progress >= step ? "bg-purple-400" : ""
-                }`}
-              ></div>
-            ))}
-          </ul>
-        </div>
-
-        {/* Personality Questions */}
-        <form className="max-w-sm mx-auto pt-5" onSubmit={submit}>
+  <div className="w-full h-full z-30 flex absolute inset-0">
+    <div className="w-2/5 h-full"></div>
+    <div className=" w-3/5 h-full rounded-tl-3xl rounded-bl-3xl bg-white shadow-2xl">
+      <div className="mb-5 relative  p-5 flex-col mb-2 items-center justify-between ">
+        <ul className="flex relative ">
+          {[1, 2, 3].map((step) => (
+            <div
+              key={step}
+              className={`w-full bg-gray-300 rounded-full h-2 transition-all duration-1000 ease-in-out ${
+                progress >= step ? "bg-purple-400" : ""
+              }`}
+            ></div>
+          ))}
+        </ul>
+        <p className="text-white">
+          {currentQuestionIndex + 1}/{questionList.length}
+        </p>
+      </div>
+      <div className="flex items-center w-full h-2/5 justify-center">
+        <form className="w-3/5 h-3/5 pt-5" onSubmit={submit}>
           {questionList.map((questionObj, questionIndex) => (
-            <div key={questionIndex} className="mb-5">
-              <p className="text-gray-900 dark:text-white font-medium mb-2">
-                {`Personality Question ${questionIndex + 1}: ${questionObj.question}`}
-              </p>
-              <div className="flex flex-col space-y-2">
+            <div key={questionIndex} className={questionIndex === currentQuestionIndex ? '' : 'hidden'}>
+              <div className="flex items-center justify-center">
+                <p className="text-[#191D63] font-bold mb-5">
+                  {questionObj.question}
+                </p>
+              </div>
+              <div className="flex flex-col space-y-3">
                 {questionObj.options.map((option, optionIndex) => (
-                  <label key={optionIndex} className="flex items-center">
+                  <label key={optionIndex} className="flex shadow w-full h-[50px] bg-[#F4F3F6] rounded p-1 items-center">
                     <input
                       type="radio"
                       name={`question${questionIndex}`}
                       value={optionIndex + 1}
                       checked={personality[questionIndex] === optionIndex + 1}
                       onChange={() => handleOptionChange(questionIndex, optionIndex)}
-                      className="form-radio h-4 w-4 text-teal-600 transition duration-150 ease-in-out"
+                      className="form-radio h-4 w-4 text-[#7179C6] transition duration-150 ease-in-out"
                     />
-                    <span className="ml-2">{option}</span>
+                    <span className="ml-2 font-semibold">{option}</span>
                   </label>
                 ))}
               </div>
@@ -220,15 +245,40 @@ const SignUpPage3 = () => {
           {errorMessage && (
             <p className="text-red-500 mb-3">{errorMessage}</p>
           )}
-          <button
-            type="submit"
-            className="text-white bg-teal-400 hover:bg-teal-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
-            Submit
-          </button>
+          <div className="flex items-center justify-center">
+          <div className="w-2/5">
+            <div class="flex justify-end mb-1 mt-3">
+              <span class="text-sm font-medium text-[#757575] dark:text-white">{currentQuestionIndex + 1}/{questionList.length}</span>
+            </div>
+            <div class="w-full bg-[#EDE8E3] rounded-full h-2.5 dark:bg-gray-700">
+              <div class="bg-[#31CD63] h-2.5 rounded-full" style={{width: `${((currentQuestionIndex + 1) / questionList.length) * 100}%`}}></div>
+            </div>
+          </div>
+          </div>
+          <div className="flex justify-end mt-10">
+            {currentQuestionIndex === questionList.length - 1 ? (
+              <button
+                type="submit"
+                className="text-white bg-[#7179C6] cursor-pointer hover:bg-purple-500 focus:ring-4 focus:outline-none focus:ring-purple-500 font-medium rounded-lg text-sm px-5 w-[100px] py-2.5 text-center"
+              >
+                Submit
+              </button>
+            ) : (
+              <button
+                type="button"
+                onClick={goToNextQuestion}
+                className="text-white bg-[#7179C6] cursor-pointer hover:bg-purple-500 focus:ring-4 focus:outline-none focus:ring-purple-500 font-medium rounded-lg text-sm px-5 w-[100px] py-2.5 text-center"
+              >
+                Next
+              </button>
+            )}
+          </div>
         </form>
       </div>
     </div>
+  </div>
+</div>
+
   );
 };
 
