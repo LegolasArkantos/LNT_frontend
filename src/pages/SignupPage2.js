a5b1ef524cbb902253b90ee98a041871e1476f1f
LegolasArkantos <unasaamir101@gmail.com>
2024-05-08 17:06:31 +0500

    firebase noe

--- src/pages/SignupPage2.js
+++ src/pages/SignupPage2.js
@@ -61,7 +61,7 @@ const [teacherFileNames, setTeacherFileNames] = useState([]);
               role,
               educationalCredential,
               educationalLevel,
-              profilePicture: reader.result,
+              profilePicture: selectedFile,
               aboutMe,
               teacherFiles,
               
@@ -73,10 +73,8 @@ const [teacherFileNames, setTeacherFileNames] = useState([]);
         console.error("AHHHHHHHH!!");
       };
     } else {
-      // Handle the case where no file is selected
       console.error("No file selected");
       setFileError("Please upload a profile picture.");
-      // You can show an error message or take other actions as needed
     }
   };
   const handleTeacherFileInputChange = (event) => {
