2fe8a866358e0401e0fb2dd99f8a7736a0343e35
Uzair Ali <uzairalinusrat@gmail.com>
2024-05-15 23:11:13 +0500

    made forgot password page

--- src/pages/ForgotPasswordPage.js
+++ src/pages/ForgotPasswordPage.js
@@ -1,59 +1,35 @@
-import { useState } from "react";
-import { api } from "../services/api";
-import { setAuthValues } from "../features/auth";
-import { useNavigate } from "react-router-dom";
-import { jwtDecode } from "jwt-decode";
-import { useDispatch } from "react-redux";
+import React from 'react'
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import logo from '../assets/lntlogo-removebg-preview.png';
+import { api } from '../services/api';
+// import { Resend } from 'resend';
 
-const Login = () => {
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
+const ForgotPasswordPage = () => {
 
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
+    const [email, setEmail] = useState("");
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);
 
-  const submit = async (e) => {
-    e.preventDefault();
-    try {
-      setLoading(true);
-      const lowercasedEmail = email.toLowerCase()
-      await api
-        .post(
-          "/auth/login",
-          {
-            email: lowercasedEmail,
-            password,
-          },
-          {
-            headers: { "Content-Type": "application/json" },
-            withCredentials: true,
-          }
-        )
-        .then((res) => {
-          if (res.status === 200) {
-            const { accessToken } = res.data;
-            console.log(accessToken);
-            const decodedToken = jwtDecode(accessToken);
-            const role = decodedToken.role;
-            dispatch(setAuthValues({ role, accessToken }));
-            if (decodedToken.role === "Admin") {
-              navigate("/admin-dashboard");
-            } else if (decodedToken.role === "Student") {
-              navigate("/student-home-page");
-            } else {
-              navigate("/teacher-home-page");
+    const navigate = useNavigate();
+
+    const submit = async (e) => {
+        e.preventDefault();
+        try {
+          setLoading(true);
+          const lowercasedEmail = email.toLowerCase()
+          await api.post("auth/forgot-password", {email: lowercasedEmail}).then((res) => {
+            if (res.status === 200) {
+                alert("Password Reset Email has been sent to the given email");
+                setLoading(false);
             }
-          }
-        });
-    } catch (error) {
-      setLoading(false)
-      console.log(error);
-      setError(error.response.data.message);
-    }
-  };
+          })
+        } catch (error) {
+          setLoading(false)
+          console.log(error);
+          setError(error.response.data.message);
+        }
+      };
 
   return (
     <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0 bg-gray-200 dark:bg-gray-900">
@@ -62,7 +38,7 @@ const Login = () => {
           <div class="flex mb-10 items-center justify-center space-x-3 rtl:space-x-reverse">
             <img src={logo} class="h-8" />
           </div>
-          <h1 class="text-2xl font-bold">Sign In to your Account</h1>
+          <h1 class="text-2xl font-bold">Forgot Password</h1>
           <form class="max-w-sm mx-auto pt-5" onSubmit={submit}>
             <div class="mb-5">
               <label
@@ -83,29 +59,7 @@ const Login = () => {
                 required
               />
             </div>
-            <div class="mb-2">
-              <label
-                for="password"
-                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-              >
-                Password
-              </label>
-              <input
-                type="password"
-                id="password"
-                value={password}
-                onChange={(e) => {
-                  setPassword(e.target.value);
-                }}
-                class="bg-gray-50 mb-2 border border-[#7179C6] text-gray-900 text-sm rounded-lg focus:ring-[#7179C6] focus:border-[#7179C6] block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="•••••••••"
-                required
-              />
-            </div>
             <div className="flex justify-end mb-3">
-            <p onClick={() => navigate("/forgot-password")} className="text-[#7179C6] font-semibold text-sm cursor-pointer hover:underline">
-            forgot password?
-            </p>
             </div>
             {error && <p className="text-red-500">{error}</p>} 
             {
@@ -124,24 +78,16 @@ Loading...
               type="submit"
               class="text-white bg-[#7179C6] hover:bg-purple-500 focus:ring-4 focus:outline-none focus:ring-purple-500 font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center"
             >
-              Sign in
+              Submit
             </button>
               )
             }
             
-            <div className="mt-3 flex items-center">
-            <p className="text-gray-600">
-            Dont have an account?
-            </p>
-          <a href="signup" className="text-sm text-[#7179C6] ml-2 font-semibold hover:underline cursor-pointer">
-          Sign up
-            </a>
-          </div>
           </form>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Login;
+export default ForgotPasswordPage
