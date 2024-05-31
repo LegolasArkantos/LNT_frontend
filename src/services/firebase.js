fa0dfa6017fcc063ac09f42eb8e1ad9a0ab88414
LegolasArkantos <unasaamir101@gmail.com>
2024-05-31 21:01:13 +0500

    fixes

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -10,16 +10,7 @@ const firebaseConfig = {
     appId: process.env.REACT_APP_FIREBASE_APP_ID,
     measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBBkvVgnjPh-_Lj2PKhb-7CU1DO8vlZc74",
-//   authDomain: "learn-and-track.firebaseapp.com",
-//   projectId: "learn-and-track",
-//   storageBucket: "learn-and-track.appspot.com",
-//   messagingSenderId: "648773157425",
-//   appId: "1:648773157425:web:17aabdaf2b95d8238dcb10",
-//   measurementId: "G-N5Q5ZERVKG"
-// };
-  
+
 
 // Initialize Firebase App
 const app = initializeApp(firebaseConfig);
